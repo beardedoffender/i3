0269bf9674d1591d5b8b63f3cdbe163511997634
Jon <johnathan.heath@gmail.com>
2017-12-11 06:27:23 -0500

    my i3 config

---
+++ layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --primary --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 0x168 --rotate normal --output DP-2 --mode 1920x1080 --pos 3000x272 --rotate normal
